4d3490db1f84f5c56a467b9dde4faa59ce835950
JALLIFIER-TALMAT Ruben <ruben.jallifier@gmail.com>
2023-01-30 16:56:59 +0100

    see meny OK

--- base/__init__.py
+++ base/__init__.py
@@ -27,28 +27,30 @@ def see_menu():
     if path.isfile(menu) is False:
         raise Exception("File not found")
 
-
     # Read JSON file
     with open(menu) as fp:
         listObj = json.load(fp)
 
-
     # Verify existing list
     print(listObj)
 
-def prog_Menu():
+
+def prog_menu():
     print("Bienvenue dans notre restaurant")
     print("----------------------------------------------------------------------")
     print("1-Menu")
     print("2-prendre une commande")
     print("3-voir la note d'une table")
 
+
 def main():
-   prog_Menu()
-   choix =input("faites votre choix : ")
-   if choix==1:
+    prog_menu()
+    choix = int(input("faites votre choix : "))
+    if choix == 1:
         see_menu()
-   if choix==2:
+    if choix == 2:
         add_order()
-   if choix==3:
+    if choix == 3:
         print("on verra la fonction pour la note")
+
+main()
