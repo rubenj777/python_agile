199c1013fafbbbace3c888709e04c27bc908dcc6
JALLIFIER-TALMAT Ruben <ruben.jallifier@gmail.com>
2023-01-30 15:53:03 +0100

    json db + json insertion OK

--- base/__init__.py
+++ base/__init__.py
@@ -1,2 +1,35 @@
-def add(a, b):
-    return a + b
+import json
+from os import path
+
+filename = "C:/Users/Dell/Desktop/formation/agile/resto/data.json"
+list = []
+
+def see_menu():
+
+    if path.isfile(filename) is False:
+        raise Exception("File not found")
+ 
+    # Read JSON file
+    with open(filename) as fp:
+        listObj = json.load(fp)
+ 
+    # Verify existing list
+    print(listObj)
+    print(type(listObj))
+ 
+    listObj.append({
+    "Menu": "menu 4",
+    "Prix": 33
+    })
+ 
+    # Verify updated list
+    print(listObj)
+ 
+    with open(filename, 'w') as json_file:
+        json.dump(listObj, json_file, 
+                        indent=4,  
+                        separators=(',',': '))
+ 
+    print('Successfully appended to the JSON file')
+
+see_menu()
