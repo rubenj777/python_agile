87d653c47cd3ef94d4fdcd568a9eb2ad8936fc2a
Mohamed KONATE <georgeskonate95@gmail.com>
2023-01-30 16:46:46 +0100

    gestion du Menu

--- base/__init__.py
+++ base/__init__.py
@@ -1,35 +1,50 @@
 import json
 from os import path
 
-filename = "data.json"
-list = []
+menu = "menu.json"
+orders = "orders.json"
 
 def add_order():
 
-    if path.isfile(filename) is False:
+    if path.isfile(orders) is False:
         raise Exception("File not found")
- 
+
+    with open(orders) as fp:
+        listOrders = json.load(fp)
+
+    listOrders.append({"Menu": "menu 4", "Prix": 33})
+
+    # Verify updated list
+    print(listOrders)
+
+    with open(orders, "w") as json_file:
+        json.dump(listOrders, json_file, indent=4, separators=(",", ": "))
+
+def see_menu():
+
+    if path.isfile(menu) is False:
+        raise Exception("File not found")
+
     # Read JSON file
-    with open(filename) as fp:
+    with open(menu) as fp:
         listObj = json.load(fp)
- 
+
     # Verify existing list
     print(listObj)
-    print(type(listObj))
- 
-    listObj.append({
-    "Menu": "menu 4",
-    "Prix": 33
-    })
- 
-    # Verify updated list
-    print(listObj)
- 
-    with open(filename, 'w') as json_file:
-        json.dump(listObj, json_file, 
-                        indent=4,  
-                        separators=(',',': '))
- 
-    print('Successfully appended to the JSON file')
 
-see_menu()
+def prog_Menu():
+    print("Bienvenue dans notre restaurant")
+    print("----------------------------------------------------------------------")
+    print("1-Menu")
+    print("2-prendre une commande")
+    print("3-voir la note d'une table")
+
+def main():
+   prog_Menu()
+   choix =input("faites votre choix : ")
+   if choix==1:
+        see_menu()
+   if choix==2:
+        add_order()
+   if choix==3:
+        print("on verra la fonction pour la note")
--- tests/test_base.py
+++ tests/test_base.py
@@ -2,4 +2,4 @@ import base
 
 
 def test_add():
-    assert base.add(1, 2) == 4
+    assert base.seeMenu()
